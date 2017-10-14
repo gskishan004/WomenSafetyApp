12877fdc1ed2136cd250b108d516abc5c000719c
apple <apple@apples-MacBook-Pro.local>
2017-10-14 13:14:03 +0530

    Working ios code

--- WomanSafety_UI/index.ios.js
+++
@@ -1,3 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-AppRegistry.registerComponent('CMB_Women_Safety', () => App);
--- WomanSafety_UI/ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/yoga.build/Objects-normal/x86_64/YGEnums.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/apple/Work/Commercial\ Banking_HSBC/CMB_Women_Safety/WomanSafety/node_modules/react-native/ReactCommon/yoga/yoga/YGEnums.c \
-  /Users/apple/Work/Commercial\ Banking_HSBC/CMB_Women_Safety/WomanSafety/node_modules/react-native/ReactCommon/yoga/yoga/YGEnums.h \
-  /Users/apple/Work/Commercial\ Banking_HSBC/CMB_Women_Safety/WomanSafety/node_modules/react-native/ReactCommon/yoga/yoga/YGMacros.h
